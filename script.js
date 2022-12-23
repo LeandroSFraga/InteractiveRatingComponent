8d46c090aaeffbae994734fa3c2f35a3657c955c
LeandroFraga99 <71570108+LeandroFraga99@users.noreply.github.com>
2022-12-23 10:45:40 -0300

    Ajustes na passagem de telas

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let choosedValue;
 let userName;
-localStorage.setItem("avaliation", 0)
-const valor = document.getElementById("btn-submit").onclick = () => {
+
+localStorage.setItem("user", '');
+localStorage.setItem("avaliation", 0);
+document.getElementById("rating-form").onsubmit = (event) => {
+    event.preventDefault();
     const radios = document.getElementsByName("nota");
     const choosedRadio = Array.from(radios).find(value => value.checked);
     choosedValue = choosedRadio.value;
     userName = document.getElementById("name").value;
     localStorage.setItem("avaliation", choosedValue);
-    localStorage.setItem("user", userName)
+    localStorage.setItem("user", userName);
+    history.pushState({}, '', '/submited.html');
+    location.reload();
 }
 
